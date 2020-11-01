84513290235f1ad5a834e5c8ecb45488566123e1
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-11-01 07:54:17 +0530

    Variables

---
+++ 02.Variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int minutesPerHour = 60;
+	const float PI = 3.14; 
+  	int x = 5, y = 6, z = 50;  
+  	cout << x + y + z <<"\n";
+	int m = 60;
+	cout << "\n M="<<m<<"\n minutesPerHour"<<minutesPerHour;
+  	return 0;
+}
