c069e7eae0d5e7875229d15d1ae31d522a33ce67
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-11-11 17:12:00 +0530

    Functions

---
+++ 20.Functions/Debug/src/20.Functions.d
@@ -0,0 +1,1 @@
+src/20.Functions.o: ../src/20.Functions.cpp
---
+++ 20.Functions/src/20.Functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void HelloFunction() {
+  cout << "Hello World CPP";
+}
+
+int main() {
+  HelloFunction();
+  return 0;
+}
